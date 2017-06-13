75966171c7d71829f2ba9a07c76d3f95fce00dd5
Mateus Zanini <mateus-zanini@hotmail.com>
2017-06-13 00:29:29 -0300

    Adicionadas fonts e layout inicial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
-function BarberShop(){
+function BarberShop() {
+    
+    $('.scrollspy').scrollSpy();
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+
+    $('.item-menu').addClass('amber-text');
+
+    this.consoleLog();
+}
+
+BarberShop.prototype.consoleLog = function () {
     
 };
 
-window.onload = function() {
-  window.barberShop = new BarberShop();
+window.onload = function () {
+    window.barberShop = new BarberShop();
 };
