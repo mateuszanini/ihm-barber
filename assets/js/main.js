d637d65e35e02218b60a7d2ac50842eb02bde817
Mateus Zanini <mateus-zanini@hotmail.com>
2017-06-13 10:05:43 -0300

    Adicionado slider e footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,25 @@ function BarberShop() {
     $('.scrollspy').scrollSpy();
     $('.parallax').parallax();
     $(".button-collapse").sideNav();
+    $('.tooltipped').tooltip({delay: 50});
+    $('.slider').slider();
 
+    //ALTERA COR DO TEXTO
     $('.item-menu').addClass('amber-text');
+
+    //SCROOL TOP
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
 
     this.consoleLog();
 }
