d5baf21e4a3dee4828a4e2f400c48bb5cded7ae9
Mateus Zanini <mateus-zanini@hotmail.com>
2017-06-20 14:23:06 +0000

    Acessibilidade com teclado e tabindex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/*global $*/
 function BarberShop() {
 
 	$('.scrollspy').scrollSpy();
@@ -15,7 +16,7 @@ function BarberShop() {
 	$('.item-menu').addClass('amber-text');
 
 	//SCROOL TOP
-	$(window).scroll(function () {
+	$(window).scroll(function() {
 		if ($(this).scrollTop() > 100) {
 			$('.scrollToTop').fadeIn();
 		}
@@ -24,7 +25,7 @@ function BarberShop() {
 		}
 	});
 
-	$('.scrollToTop').click(function () {
+	$('.scrollToTop').click(function() {
 		$('html, body').animate({
 			scrollTop: 0
 		}, 800);
@@ -32,37 +33,82 @@ function BarberShop() {
 	});
 
 	// Evento do formulário
-	$('#enviar').click(function (e) {
+	$('#enviar').click(function(e) {
 		e.preventDefault();
-		Materialize.toast('Mensagem enviada!', 3000, 'rounded');
+		Materialize.toast('Mensagem enviada!', 3000);
 	});
 
-	$('#menu-servicos').click(function () {
+	$('#menu-servicos').click(function() {
 		$('html, body').animate({
 			scrollTop: $("#servicos").offset().top - 75
 		}, 200);
 	});
-	$('#menu-produtos').click(function () {
+	$('#menu-produtos').click(function() {
 		$('html, body').animate({
 			scrollTop: $("#produtos").offset().top - 75
 		}, 200);
 	});
-	$('#menu-galeria').click(function () {
+	$('#menu-galeria').click(function() {
 		$('html, body').animate({
 			scrollTop: $("#galeria").offset().top - 75
 		}, 200);
 	});
-	$('#menu-contato').click(function () {
+	$('#menu-contato').click(function() {
 		$('html, body').animate({
 			scrollTop: $("#contato").offset().top - 75
 		}, 200);
 	});
 }
 
-BarberShop.prototype.consoleLog = function () {
+var KEYBOARD = {
+	esc: 27,
+	enter: 13,
+	left: 37,
+	right: 39,
+	pauseP: 80,
+	pausep: 112
+}
+
+var paused = 0;
+
+$(document).on('keydown', 'textarea', function(event) {
+	if (event.which == KEYBOARD.enter && (event.metaKey || event.ctrlKey)) {
+		event.preventDefault();
+		Materialize.toast('Mensagem enviada!', 3000);
+		/*$(this).parents('form').submit();*/
+	}
+});
+
+$(document).on('keyup.modal', function(event) {
+	if (event.which == KEYBOARD.esc) {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 800);
+	}
+	if (event.which == KEYBOARD.left) {
+		$('.slider').slider('prev');
+	}
+	else if (event.which == KEYBOARD.right) {
+		$('.slider').slider('next');
+	}
+	else if (event.which == KEYBOARD.pauseP || event.which == KEYBOARD.pausep) {
+		if (paused) {
+			$('.slider').slider('start');
+			paused = 0;
+			$('#indicador-slider').html('<i class="fa fa-pause" aria-hidden="true"></i>');
+		}
+		else {
+			$('.slider').slider('pause');
+			paused = 1;
+			$('#indicador-slider').html('<i class="fa fa-play" aria-hidden="true"></i>');
+		}
+	}
+})
+
+BarberShop.prototype.consoleLog = function() {
 
 };
 
-window.onload = function () {
+window.onload = function() {
 	window.barberShop = new BarberShop();
 };
